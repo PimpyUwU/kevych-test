d74d9b3d7b52ae79d726cd5490c4a675c6ed0656
PimpyUwU <pimpy.owo@gmail.com>
2025-02-27 23:37:45 +0200

    fixed port

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Authorization',
   });
 
-  const port = 3001;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
 }
 
