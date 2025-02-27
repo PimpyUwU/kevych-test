1914de7645d38b541f7c52d7136f699cc44d320f
PimpyUwU <pimpy.owo@gmail.com>
2025-02-27 23:13:08 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import {ValidationPipe} from "@nestjs/common";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cookieParser())
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
   }));
 
-  const port = process.env.PORT || 3000;
+  app.enableCors({
+    origin: 'https://kevych-test-front.vercel.app',
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
 
+  const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
 }
 
-bootstrap()
+bootstrap();
